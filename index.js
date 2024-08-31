94bbebaa02a633afa827d6432b29fbe69bcdfa53
=Mudit Gulgulia <gulgulia17@gmail.com>
2024-08-31 21:18:00 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,64 @@
+import { join } from "path";
+import { readFileSync } from "fs";
+import express from "express";
+import serveStatic from "serve-static";
+import path from "path";
+import shopify from "./shopify.js";
+import PrivacyWebhookHandlers from "./privacy.js";
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+const PORT = parseInt(
+  process.env.BACKEND_PORT || process.env.PORT || "3000",
+  10
+);
+
+const STATIC_PATH =
+  process.env.NODE_ENV === "production"
+    ? `${process.cwd()}/frontend/dist`
+    : `${process.cwd()}/src/`;
+
+const app = express();
+
+// Set up Shopify authentication and webhook handling
+app.get(shopify.config.auth.path, shopify.auth.begin());
+app.get(
+  shopify.config.auth.callbackPath,
+  shopify.auth.callback(),
+  shopify.redirectToShopifyOrAppRoot()
+);
+app.post(
+  shopify.config.webhooks.path,
+  shopify.processWebhooks({ webhookHandlers: PrivacyWebhookHandlers })
+);
+
+// If you are adding routes outside of the /api path, remember to
+// also add a proxy rule for them in web/frontend/vite.config.js
+
+app.use("/api/*", shopify.validateAuthenticatedSession());
+
+app.use(express.json());
+
+app.get("/api/orders/all", async (_req, res) => {
+  const orderData = await shopify.api.rest.Order.all({
+    session: res.locals.shopify.session,
+    status: "any"
+  });
+  res.status(200).send(orderData);
+  console.log("order-date")
+});
+
+
+
+app.use(shopify.cspHeaders());
+app.use(serveStatic(STATIC_PATH, { index: false }));
+
+app.use("/*", shopify.ensureInstalledOnShop(), async (_req, res, _next) => {
+  return res
+    .status(200)
+    .set("Content-Type", "text/html")
+    .send(readFileSync(join(STATIC_PATH, "index.html")));
+});
+
+app.listen(PORT);
---
+++ src/assets/tslib.es6-ebcd357f.js
@@ -0,0 +1,1 @@
+import{g as e,t as s}from"./index-46840a41.js";const r=e(s);export{r};
